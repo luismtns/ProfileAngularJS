1b41517ab5fa64de04b76121dff96f59877d82ef
Luis Otávio <luismtns@gmail.com>
2018-11-25 18:34:44 -0200

    Pesquisar finalizado e loader implementado

--- js/controller/pesquisar.js
+++ js/controller/pesquisar.js
@@ -3,10 +3,19 @@ ngapp.controller("pesquisarCTRL", function ($scope, $route) {
     AOS.refresh();
     verificarLogin();
 
-    $('.filtros-pesquisar').slick({
-        infinite: false,
-        slidesToShow: 2.5,
-        slidesToScroll: 1,
-        arrows: false
-      });
+    setTimeout(function(){
+        $('#filtros-pesquisar').removeClass('hidden');
+        $('#filtros-pesquisar').slick({
+            infinite: false,
+            slidesToShow: 2.5,
+            slidesToScroll: 1,
+            arrows: false
+        });
+
+        for (let i = 0; i < 5; i++) {
+            var $boxPerfil = $( ".perfil-box").first().clone();
+            $boxPerfil.appendTo(".body-pesquisar");
+        }
+     }, 100);
+    
 });
--- js/module/module.js
+++ js/module/module.js
@@ -1,1 +1,18 @@
 var ngapp = angular.module("ngProfile", ['ngRoute']);
+
+ngapp.controller('mainCTRL', function($rootScope,$scope, $timeout) {
+    $rootScope.showSpin = true;
+
+    $rootScope.$on('$routeChangeStart', function(){
+        $timeout(function(){
+            $rootScope.showSpin = true;
+            $scope.showSpin = true;
+        }, 200);
+    });
+    $rootScope.$on('$routeChangeSuccess', function(){
+        $timeout(function(){
+            $rootScope.showSpin = false;
+            $scope.showSpin = false;
+        }, 200);
+    });
+})
